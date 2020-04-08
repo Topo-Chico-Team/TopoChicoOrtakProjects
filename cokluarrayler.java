2993c879c3da2ba7a18af7069826bafe205d3eba
cevdetabbas <50247064+cevdetabbas@users.noreply.github.com>
2020-04-07 21:52:36 -0500

    Create cokluarrayler.java

---
+++ cokluarrayler.java
@@ -0,0 +1,12 @@
+public class cokluarrayler
+{
+    public static void main(String[] args) {
+        int[][] array3= {{20,30,40},{70,80,90}};
+        //System.out.println(array3[1][2]);
+        System.out.println("----------------");
+        System.out.println("| " + array3[0][0] + " | " + array3[0][1] + " | " + array3[0][2] + " |");
+        System.out.println("----------------");
+        System.out.println("| " + array3[1][0] + " | " + array3[1][1] + " | " + array3[1][2] + " |");
+        System.out.println("----------------");
+    }
+}
